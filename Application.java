bc6757da6f5921c811b3761ee2c3be4f5b12d9c6
Hari Krishna Manupadi <harikrishnacsse@gmail.com>
2018-12-15 10:22:22 +0530

    Create Application.java
    Create Application

---
+++ Application.java
@@ -0,0 +1,22 @@
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com")
+public class Application implements CommandLineRunner {{
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+	
+	@Bean
+	public ApplicationClient getService(){
+		return  new ApplicationClient();
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		getService().callPreLoadScript();
+	}
+}
